1c58cfd5cf451ac8a73c0f38a88f396c364ee1e6
Assafuahdarrol <dpkassafuah@st.ug.edu.gh>
2024-06-17 18:56:46 +0000

    Commit Abstract Classes and Methods

---
+++ Abstract Classes and Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Circle and Rectangle
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle(4, 3);
+
+        // Display areas using GetArea()
+        Console.WriteLine($"Circle Area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+    }
+}
